a2b82ae6eb79543647c8565c4317490d7963f1f8
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-11-23 22:11:32 +0100

    feat: brand colors

---
+++ lib/src/styles/fullfuel_colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class FullfuelColors {
+  static final primary = Color(0xFF301549);
+  static final secondary = Color(0x888f9eb7);
+  static final secondary_30 = Color(0xdddde2e9);
+  static final secondary_20 = Color(0xeeebedf0);
+  static final secondary_5 = Color(0xfff9fafc);
+  static final action = Color(0xeeeb007f);
+  static final up = Color(0xffff3100);
+  static final down = Color(0xff13bf0c);
+  static final g95 = Color(0xff2a9f6d);
+  static final g98 = Color(0xff97d165);
+  static final gasoil = Color(0xff000000);
+}
