10f3e862c78b9bd6687c0c2f05af615861a1f8b6
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-12-02 23:17:08 +0100

    chore: added dotenv dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fullfuel_app/src/app.dart';
 import 'package:fullfuel_app/src/database/Hive/database.dart';
 
 void main() {
+  loadDotEnv();
   Database.init();
   runApp(App());
+}
+
+Future loadDotEnv() async {
+  await DotEnv().load('.env');
 }
