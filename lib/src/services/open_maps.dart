fa2ef1d1d340c3a8a241777179c860ac564aefd0
Pedro Medina Márquez <medinamarquezp@gmail.com>
2021-01-07 23:02:47 +0100

    feat: open on google maps service

---
+++ lib/src/services/open_maps.dart
@@ -0,0 +1,15 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class OpenMaps {
+  OpenMaps._();
+
+  static Future<void> open(double latitude, double longitude) async {
+    String googleUrl =
+        'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+    if (await canLaunch(googleUrl)) {
+      await launch(googleUrl);
+    } else {
+      throw 'Could not open the map.';
+    }
+  }
+}
