6d0ec347c270838a496f25e69634daacca106d73
Pedro Medina Márquez <medinamarquezp@gmail.com>
2021-01-07 23:02:05 +0100

    feat: brand icon widget

---
+++ lib/src/widgets/brand_icon_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fullfuel_app/src/styles/fullfuel_colors.dart';
+
+class BrandIconWidget extends StatelessWidget {
+  final String brandLogo;
+  final String size;
+
+  const BrandIconWidget({Key key, this.brandLogo = "", this.size = "SMALL"})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _radius = (size == "BIG") ? 30.0 : 22.0;
+    final _iconSize = (size == "BIG") ? 24.0 : 18.0;
+    final _apiURL = DotEnv().env['API_URL'];
+    final fuelStationIcon = "lib/assets/gasStationIcon.svg";
+    final noBrandIcon = CircleAvatar(
+      radius: _radius,
+      backgroundColor: FullfuelColors.secondary_20,
+      child: SvgPicture.asset(fuelStationIcon,
+          alignment: Alignment.center, width: _iconSize),
+    );
+    final brandIcon = (brandLogo == "")
+        ? noBrandIcon
+        : CircleAvatar(
+            backgroundImage: NetworkImage(_apiURL + brandLogo),
+            backgroundColor: Colors.transparent);
+    return Container(
+      margin: EdgeInsets.only(right: 10),
+      child: brandIcon,
+    );
+  }
+}
